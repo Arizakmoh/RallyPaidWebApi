ba3fe41a764c74fcbfb0a12b3a010817ffc10aa5
Abdirizak <70087409+Arizakmoh@users.noreply.github.com>
2022-01-09 08:19:26 +0300

    RRW-1- feature : Create car

---
+++ WebUI/WebUI/Features/Cars/CarController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebUI.Features.Cars
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Car>> Getcars()
+        {
+            var cars = new List<Car>();
+
+            var car1 = new Car
+            {
+                TeamName = "Mohamed",
+                Speed = 200,
+                MulfuctionChance = 0.2
+            };
+            var car2 = new Car
+            {
+                TeamName = "Ashraf",
+                Speed = 90,
+                MulfuctionChance = 0.3
+            };
+
+            cars.Add(car1);
+            cars.Add(car2);
+
+            return Ok(cars);
+
+        }
+    }
+}

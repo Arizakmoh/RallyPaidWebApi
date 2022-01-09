9e3f35da220f9d6ea467bc2a9715339c5b5ccdee
Abdirizak <70087409+Arizakmoh@users.noreply.github.com>
2022-01-09 09:00:38 +0300

    RRW-11 Features : Cars Endpoint

--- WebUI/WebUI/Features/Cars/CarController.cs
+++ WebUI/WebUI/Features/Cars/CarController.cs
@@ -25,12 +25,61 @@ namespace WebUI.Features.Cars
                 Speed = 90,
                 MulfuctionChance = 0.3
             };
-
             cars.Add(car1);
             cars.Add(car2);
 
             return Ok(cars);
+        }
 
+        [HttpGet]
+        [Route("{Id}")]
+        public ActionResult<List<Car>> Getcars(int id)
+        {
+            var cars = new List<Car>();
+            var car1 = new Car
+            {
+                TeamName = "Team B",
+                Speed = 100,
+                MulfuctionChance = 0.2
+            };
+
+            return Ok(car1);
+        }
+
+        [HttpPost]
+        public ActionResult<Car> CreateCar(Car car)
+        {
+            var newcar = new Car
+            {
+                Id = car.Id,
+                TeamName = car.TeamName,
+                Speed = car.Speed,
+                MulfuctionChance = car.MulfuctionChance
+            };
+
+            return newcar;
+        }
+        
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult<Car> UpdateCar(Car car)
+        {
+            var updateCar = new Car
+            {
+                Id = car.Id,
+                TeamName = car.TeamName,
+                Speed = car.Speed,
+                MulfuctionChance = car.MulfuctionChance
+            };
+
+            return updateCar;
+        }
+        [HttpDelete]
+        [Route("{id}")]
+
+        public ActionResult deleteCar(int Id)
+        {
+            return Ok($"car with id {Id} is succcessully deleted");
         }
     }
 }
